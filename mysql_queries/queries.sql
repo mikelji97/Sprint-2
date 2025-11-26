4fb4851562606e01886fdb316d53383a3b3e77fe
Mikel Jiménez Álvarez <mikel.falcesino@gmail.com>
2025-11-26 01:26:45 +0100

    Refactor SQL queries for clarity and consistency
    Add new queries to complete the exercise.

--- mysql_queries/queries.sql
+++ mysql_queries/queries.sql
@@ -40,43 +40,69 @@ SELECT nombre,precio FROM producto ORDER BY precio ASC LIMIT 1;
 SELECT nombre,precio FROM producto ORDER BY precio DESC LIMIT 1;
 -- 20 --
 SELECT nombre, codigo_fabricante FROM producto WHERE codigo_fabricante = 2 ;
+-- 21 --
+SELECT pr.nombre AS Nombre, f.nombre AS Fabricante, pr.precio AS Precio FROM producto pr JOIN fabricante f ON f.codigo = pr.codigo_fabricante;
+--22--
+SELECT pr.nombre AS Nombre, f.nombre AS Fabricante, pr.precio AS Precio FROM producto pr JOIN fabricante f ON f.codigo = pr.codigo_fabricante ORDER BY f.nombre ASC;
+--23--
+SELECT pr.codigo AS Codigo, pr.nombre AS Nombre, f.codigo AS 'C.Fabricante', f.nombre AS 'Nombre Fabricante' FROM producto pr JOIN fabricante f ON f.codigo = pr.codigo_fabricante;
+--24--
+SELECT p.nombre AS 'Nombre producto',p.precio AS Precio, f.nombre AS Fabricante FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante ORDER BY Precio ASC LIMIT 1;
+--25--
+SELECT p.nombre AS 'Nombre producto',p.precio AS Precio, f.nombre AS Fabricante FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante ORDER BY Precio DESC LIMIT 1;
+--26--
+SELECT p.nombre AS 'Productos Lenovo' FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo';
+--27--
+SELECT p.nombre AS 'Productos Crucial', p.precio AS Precio FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Crucial' AND p.precio > 200 ORDER BY p.precio ASC;
+--28--
+SELECT p.nombre AS Producto, f.nombre AS Fabricante  FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Hewlett-Packard' OR f.nombre = 'Asus' OR f.nombre = 'Seagate' 
+--29--
+SELECT p.nombre AS Producto, f.nombre AS Fabricante  FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre IN('Hewlett-Packard','Asus','Seagate');
+--30--
+SELECT p.nombre AS Producto, p.precio AS Precio  FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE p.nombre LIKE'%e';
+--31--
+SELECT p.nombre AS Producto, p.precio AS Precio  FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE p.nombre LIKE'%w%';
+--32--
+SELECT p.nombre AS Producto, p.precio AS Precio, f.nombre AS Fabricante FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE Precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
+
+
+
+
 
 -- Consultas universidad --
 USE universidad;
 -- 1 --
 SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1 ASC, apellido2 ASC, nombre ASC;
 -- 2 --
-SELECT apellido1, apellido2, nombre, telefono FROM persona WHERE tipo = 'alumno' AND telefono = '' OR telefono is NULL;
+SELECT nombre, apellido1,apellido2,tipo FROM persona WHERE tipo = 'alumno' AND telefono is NULL;
 -- 3 --
-SELECT apellido1, apellido2, nombre, fecha_nacimiento FROM persona WHERE tipo = 'alumno' AND (fecha_nacimiento >= '1999/01/01' AND fecha_nacimiento <= '1999/12/31');
+SELECT apellido1, apellido2, nombre, fecha_nacimiento FROM persona WHERE tipo = 'alumno' AND YEAR(fecha_nacimiento) = 1997;
 -- 4 --
 SELECT apellido1, apellido2, nombre, telefono, tipo, nif FROM persona WHERE tipo = 'profesor' AND (telefono = '' OR telefono is NULL)AND LOWER(nif) LIKE '%k';
 -- 5 --
-SELECT curso, cuatrimestre, id_grado FROM asignatura WHERE curso = 3 AND (cuatrimestre = 1) AND (id_grado  = 7); 
+SELECT nombre AS `Asignatura`,curso AS `Tercer curso`,cuatrimestre AS `1er Cuatrimestre`,id_grado FROM asignatura WHERE curso = 3 AND cuatrimestre = 1 AND id_grado = 7; 
 -- 6 --
 SELECT persona.apellido1, persona.apellido2, persona.nombre, departamento.nombre AS 'nombre_asignatura' FROM profesor INNER JOIN persona ON persona.id = profesor.id_profesor INNER JOIN departamento ON departamento.id = profesor.id_departamento ORDER BY persona.apellido1,persona.apellido2,persona.nombre ASC;
 -- 7 --
-SELECT p.nombre AS Nombre, a.nombre, ce.anyo_inicio,ce.anyo_fin FROM asignatura a JOIN alumno_se_matricula_asignatura am ON a.id = am.id_alumno JOIN persona p ON am.id_alumno = p.id JOIN curso_escolar ce ON p.id = ce.id WHERE p.nif= '26902806M';
+SELECT p.nombre AS Nombre, p.nif AS DNI, a.nombre AS Asignatura, ce.anyo_inicio AS 'Año de inicio',ce.anyo_fin AS 'Año de fin'FROM asignatura a JOIN alumno_se_matricula_asignatura am ON a.id = am.id_asignatura JOIN persona p ON am.id_alumno = p.id JOIN curso_escolar ce ON am.id_curso_escolar = ce.id WHERE p.nif = '26902806M';
 -- 8 --
-SELECT d.nombre FROM departamento d JOIN profesor p ON d.id = p.id_departamento JOIN asignatura a ON p.id_profesor = a.id_profesor JOIN grado g ON a.id_grado = g.id WHERE g.nombre = 'Grado en Ingeniería Agrícola (Plan 2015)';  
+SELECT DISTINCT d.nombre AS 'Nombre Dep.' FROM departamento d JOIN profesor p ON d.id = p.id_departamento JOIN asignatura a ON a.id_profesor = p.id_profesor JOIN grado g ON g.id = a.id_grado WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
 -- 9 --
-
-SELECT DISTINCT p.nombre, p.apellido1, p.apellido2 FROM persona p JOIN alumno_se_matricula_asignatura ams ON p.id = ams.id_alumno JOIN curso_escolar ce ON ams.id_curso_escolar = ce.id WHERE ce.anyo_inicio = 2018; 
+SELECT DISTINCT p.id AS Numero, p.nombre AS Nombre, p.apellido1 AS 'Primer apellido', p.apellido2 AS 'Segundo apellido',ce.anyo_inicio AS 'Año de inicio', ce.anyo_fin AS 'Año fin' FROM persona p JOIN alumno_se_matricula_asignatura am ON p.id = am.id_alumno JOIN curso_escolar ce ON am.id_curso_escolar = ce.id WHERE ce.anyo_inicio = 2018 AND CE.anyo_fin = 2019;
 
 -- Resuelve las 6 siguientes consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN --
 
 -- 1 --
-SELECT d.nombre, pr.apellido1, pr.apellido2, pr.nombre FROM profesor p LEFT JOIN departamento d ON p.id_departamento = d.id JOIN persona pr ON p.id_profesor = pr.id ORDER BY d.nombre ASC, pr.apellido1 ASC, pr.apellido2 ASC, pr.nombre ASC;
+SELECT d.nombre AS Departamento, pr.apellido1 AS 'Primer apellido', pr.apellido2 AS 'Segundo apellido', pr.nombre AS Nombre FROM profesor p LEFT JOIN departamento d ON p.id_departamento = d.id LEFT JOIN persona pr ON p.id_profesor = pr.id ORDER BY d.nombre ASC, pr.apellido1 ASC, pr.apellido2 ASC, pr.nombre ASC;
 -- 2 --
-SELECT d.nombre, pr.apellido1, pr.apellido2, pr.nombre FROM profesor p LEFT JOIN departamento d ON p.id_departamento = d.id JOIN persona pr ON p.id_profesor = pr.id WHERE d.id IS NULL ORDER BY d.nombre ASC, pr.apellido1 ASC, pr.apellido2 ASC, pr.nombre ASC;
+SELECT p.apellido1 AS 'Primer apellido', p.apellido2 AS 'Segundo apellido',p.nombre AS Nombre FROM persona p JOIN profesor pr ON pr.id_profesor = p.id LEFT JOIN departamento d ON pr.id_departamento = d.id WHERE d.id IS NULL; 
 -- 3 --
-SELECT d.nombre FROM profesor p RIGHT JOIN departamento d ON p.id_departamento = d.id WHERE p.id_profesor IS NULL;
+SELECT dp.nombre AS Departamento, pr.id_profesor AS 'Id Profesor' FROM departamento dp LEFT JOIN profesor pr ON dp.id = pr.id_departamento WHERE pr.id_profesor IS NULL;
 -- 4 --
-SELECT a.nombre, pr.apellido1, pr.apellido2, pr.nombre FROM profesor p LEFT JOIN asignatura a ON p.id_profesor = a.id_profesor JOIN persona pr ON p.id_profesor = pr.id WHERE a.id_profesor IS NULL;
+SELECT p.nombre AS Nombre, p.apellido1 AS 'Primer apellido', p.apellido2 AS 'Segundo apellido',a.nombre AS asignatura, p.tipo AS Tipo FROM persona p  JOIN profesor pr ON p.id = pr.id_profesor LEFT JOIN asignatura a ON a.id_profesor = pr.id_profesor WHERE a.id IS NULL;
 -- 5 --
 SELECT a.nombre FROM asignatura a LEFT JOIN profesor p ON a.id_profesor = p.id_profesor WHERE p.id_profesor IS NULL;
--- 6 --
-SELECT d.nombre FROM departamento d LEFT JOIN profesor p ON d.id = p.id_departamento LEFT JOIN asignatura a ON p.id_profesor = a.id_profesor WHERE a.id_profesor IS NULL;
+
 
 -- Consultas generales --
 
@@ -85,10 +111,20 @@ SELECT COUNT(*) AS Total FROM persona WHERE tipo = 'alumno' ;
 -- 2 --
 SELECT COUNT(*) AS Total FROM persona WHERE fecha_nacimiento >= '1999-01-01' AND fecha_nacimiento <= '1999-12-31';
 -- 3 --
-SELECT d.nombre AS Departamento, COUNT(p.id_profesor) AS Total FROM profesor p  INNER JOIN departamento d ON p.id_departamento = d.id WHERE p.id_profesor IS NOT NULL ORDER BY COUNT(p.id_profesor) ASC;
+SELECT dp.nombre AS Departamento, COUNT(pr.id_profesor) AS Total FROM departamento dp INNER JOIN profesor pr ON dp.id = pr.id_departamento GROUP BY dp.id, dp.nombre ORDER BY Total DESC;
 -- 4 --
-SELECT d.nombre, COUNT(p.id_profesor) AS profesores FROM departamento d LEFT JOIN profesor p ON d.id = p.id_departamento;
+SELECT dp.nombre AS Departamentos, COUNT(pr.id_profesor) AS Profesores FROM departamento dp LEFT JOIN profesor pr ON pr.id_departamento = dp.id GROUP BY dp.id, dp.nombre;
 -- 5 --
-SELECT g.nombre, COUNT(a.id) AS Total FROM grado g LEFT JOIN asignatura a ON g.id = a.id_grado ORDER BY Total DESC;
+SELECT gr.nombre AS Grado, COUNT(a.id) AS Asignaturas FROM grado gr LEFT JOIN asignatura a ON a.id_grado = gr.id GROUP BY gr.nombre  ORDER BY Asignaturas DESC;
+-- 6--
+SELECT gr.nombre AS Grado, COUNT(a.id) AS asignatura FROM grado gr JOIN asignatura a ON gr.id = a.id_grado GROUP BY gr.nombre HAVING COUNT(a.id) >= 40;
+-- 7 --
+SELECT gr.nombre AS Grado, a.tipo AS 'Tipo asignatura', SUM(a.creditos) AS 'Total creditos' FROM grado gr JOIN asignatura a ON a.id_grado = gr.id GROUP BY gr.nombre, a.tipo;
+-- 8 --
+SELECT ce.anyo_inicio AS 'Año de inicio', COUNT(DISTINCT p.id) AS Alumnos FROM persona p JOIN alumno_se_matricula_asignatura am ON am.id_alumno = p.id JOIN curso_escolar ce ON ce.id = am.id_curso_escolar GROUP BY ce.anyo_inicio;
+--9--
 
+--10--
+
+--11--
 
